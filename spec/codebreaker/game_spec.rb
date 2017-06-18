412d6bc3835f1a4087f134019a73f42a2f78556d
kh0ma-1991 <kh0ma@yahoo.com>
2017-06-18 23:21:58 +0300

    refactoring

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -3,37 +3,35 @@ require_relative 'test_data'
 
 module Codebreaker
   RSpec.describe Game do
-    let(:game) { Game.new }
-    let(:ch) { ConsoleHelper }
     describe '#start' do
       before do
-        game.start
+        subject.start
       end
 
       it 'saves secret code' do
-        expect(game.instance_variable_get(:@secret_code)).not_to be_empty
+        expect(subject.instance_variable_get(:@secret_code)).not_to be_empty
       end
 
       it 'saves 4 numbers secret code' do
-        expect(game.instance_variable_get(:@secret_code).size()).to eq(4)
+        expect(subject.instance_variable_get(:@secret_code).size()).to eq(4)
       end
 
       it 'saves secret code with numbers from 1 to 6' do
-        expect(game.instance_variable_get(:@secret_code)).to match(/([1-6]+){4}/)
+        expect(subject.instance_variable_get(:@secret_code)).to match(/([1-6]+){4}/)
       end
 
       it 'call rand function' do
-        allow(game).to receive(:rand).and_return(1)
-        expect(game).to receive(:rand)
-        game.start
+        allow(subject).to receive(:rand).and_return(1)
+        expect(subject).to receive(:rand)
+        subject.start
       end
     end
 
     describe '#check_code' do
       shared_examples 'code_checker' do |secret,guess,result|
         it "return '#{result}' when secret code = #{secret}, and guess code = #{guess}" do
-          game.instance_variable_set(:@secret_code, secret)
-          expect(game.check_code(guess)).to eq(result)
+          subject.instance_variable_set(:@secret_code, secret)
+          expect(subject.check_code(guess)).to eq(result)
         end
       end
 
